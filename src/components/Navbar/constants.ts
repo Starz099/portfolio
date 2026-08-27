f0546ede7d53be1583c99f3d2c6679d545bcc0b3
Starz099 <mayankjod016@gmail.com>
2026-08-27 20:21:43 +0530

    add resume link to navbar

--- src/components/Navbar/constants.ts
+++ src/components/Navbar/constants.ts
@@ -2,7 +2,7 @@ export const linkItems = [
   { href: "/", label: "Home" },
   { href: "/projects", label: "Work" },
   { href: "/blog", label: "Blog" },
-  // { href: "/work", label: "Work" },
   // { href: "/components/boombot", label: "Components" },
+  { href: "/resume", label: "Resume" },
   { href: "/contact", label: "Contact" },
 ];
